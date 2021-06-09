8105e4b4d9df50c8ab1334d4457c46a4164fb6f8
osongo <osongodonotieno@gmail.com>
2021-06-09 18:15:51 +0300

    impliment basic math operation

--- basicMath.js
+++ basicMath.js
@@ -20,3 +20,27 @@ function divide (a,b) {
 function subtract (a,b) {
     return a - b;
 }
+
+/*create a new function operate that takes operator and two numbers
+ and then calls on the above function on the numbers
+*/
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+//call multiply on the operate function
+let product=operate(multiply,30,2)
+console.log(product);
+
+//call addition
+let sum=operate(add,30,2)
+console.log(sum);
+
+//call subtraction
+let diff=operate(subtract,30,2)
+console.log(diff)
+
+//call divide
+let div=operate(divide,30,2)
+console.log(div)
